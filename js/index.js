9a5a9672ab526afb0a5b922443659272dc72253f
Ganesh Khirwadkar <ganeshkhirwadkar@Ganeshs-MacBook-Pro.local>
2022-10-23 03:44:30 +0530

    Refactoring

--- js/index.js
+++ js/index.js
@@ -2,31 +2,35 @@
 import { sort, shuffle, render, clearGrid, CARDS_CONFIG } from "./helpers.js";
 
 // Listeners
-const handleShuffle = (grid, cards) => {
+function handleShuffle(grid, cards) {
   clearGrid(grid);
   const shuffledCards = shuffle(cards);
   render(grid, shuffledCards);
-};
+}
 
-const handleSort = (grid, cards) => {
+function handleSort(grid, cards) {
   clearGrid(grid);
-  const sortedCards = sort(cards);
-  render(grid, sortedCards);
-};
-
-function loadScreen() {
-  const cards = [...CARDS_CONFIG];
-
-  const grid = document.getElementsByClassName("grid")[0];
-
-  const shuffleControl = document.getElementById("shuffle");
-  const sortControl = document.getElementById("sort");
-
-  shuffleControl.addEventListener("click", () => handleShuffle(grid, cards));
-  sortControl.addEventListener("click", () => handleSort(grid, cards));
-
   const sortedCards = sort(cards);
   render(grid, sortedCards);
 }
 
-window.onload = loadScreen;
+function addListeners(grid, cards) {
+  const shuffleControl = document.getElementById("shuffle");
+  const sortControl = document.getElementById("sort");
+  shuffleControl.addEventListener("click", () => handleShuffle(grid, cards));
+  sortControl.addEventListener("click", () => handleSort(grid, cards));
+}
+
+function initialRender(grid, cards) {
+  const sortedCards = sort(cards);
+  render(grid, sortedCards);
+}
+
+function initialise() {
+  const cards = [...CARDS_CONFIG];
+  const grid = document.getElementsByClassName("grid")[0]; // Here only one grid element is present wrt class Name
+  addListeners(grid, cards);
+  initialRender(grid, cards);
+}
+
+window.onload = initialise;
